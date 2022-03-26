8ea786037b56634e5ac9098d29fab1b1b2475361
nxNghia <nghia1421999@gmail.com>
2022-03-26 14:27:49 +0700

    user-information page

--- client/src/Routers/index.js
+++ client/src/Routers/index.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { SignIn, SignUp } from '../Shared/Pages/Authenticate';
+import UserInformation from '../Shared/Pages/User-information';
 
 const PageRouter = () => {
     return (
@@ -8,6 +9,7 @@ const PageRouter = () => {
             <Routes>
                 <Route path='/signin' exact element={<SignIn />} />
                 <Route path='/signup' exact element={<SignUp />} />
+                <Route path='/info' exact element={<UserInformation />} />
             </Routes>
         </Router>
     )
